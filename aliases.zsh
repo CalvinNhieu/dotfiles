96ae38762691e3298085af86cb3f669c0a7e8716
Calvin Quan-Luan Nhieu <cqnhieu@uwaterloo.ca>
2016-05-04 17:21:07 -0700

    zsh config

---
+++ aliases.zsh
@@ -0,0 +1,20 @@
+alias -g al='$ZSH_CUSTOM/aliases.zsh'
+alias -g class='~/Documents/3A && ls'
+alias -g cqnhieu_cs='cqnhieu@linux.student.cs.uwaterloo.ca'
+alias -g cqnhieu_ece='cqnhieu@ecelinux.uwaterloo.ca'
+
+alias atom='open -a Atom'
+
+alias 465='~/Documents/3A/465SE && ls'
+alias 350='~/Documents/3A/350SE && ls'
+alias 349='~/Documents/3A/349CS && ls'
+alias 341='~/Documents/3A/341CS && ls'
+alias 370='~/Documents/3A/370CS && ls'
+
+alias gs='git status'
+alias gb='git branch'
+alias gpum='git pull upstream master'
+alias gpom='git push origin master'
+alias gc='git checkout'
+alias cl='clear'
+alias get349svn='svn checkout svn://ubuntu1404-004.student.cs.uwaterloo.ca:6002/cs349_repo/cqnhieu'
